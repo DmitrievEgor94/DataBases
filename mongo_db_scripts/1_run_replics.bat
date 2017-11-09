4b54cb70dfbc967c35a27ef18a319ff5be414005
Dmitriev Egor <DmitrievEgor94@yandex.ru>
2017-11-09 17:53:34 +0400

    Added time_delay in 1_run_replics.bat

--- mongo_db_scripts/1_run_replics.bat
+++ mongo_db_scripts/1_run_replics.bat
@@ -5,3 +5,5 @@ call mkdirs.bat
 start "" "%mongo_dir%\mongod.exe" --port 27017 --dbpath "%mongo_data_dir%\db\%db_name%\rs0-0" --replSet rs0 --smallfiles --oplogSize 128
 start "" "%mongo_dir%\mongod.exe" --port 27018 --dbpath "%mongo_data_dir%\db\%db_name%\rs0-1" --replSet rs0 --smallfiles --oplogSize 128
 start "" "%mongo_dir%\mongod.exe" --port 27019 --dbpath "%mongo_data_dir%\db\%db_name%\rs0-2" --replSet rs0 --smallfiles --oplogSize 128
+
+TIMEOUT /T 10
